0b33bea7e125c42b7429b6fd80df32eb43736367
mohammad uvais <mohammaduvais517@gmail.com>
2024-09-30 20:22:57 +0530

    Set Link for home page on clicking logo

--- coaching_frontend/src/Components/Navigation/Logo.jsx
+++ coaching_frontend/src/Components/Navigation/Logo.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import logo from '../../components/SvgIcons/logo.svg';
+import { Link } from 'react-router-dom';
 
 
 export const Logo = () => {
     return (
       <div className="h-24 flex justify-center items-center gap-3">
-        <img src={logo} alt="logo" className="w-24 lg:w-12 fill-white" />
+      <Link to="/"><img src={logo} alt="logo" className="w-24 lg:w-12 fill-white" /></Link>  
         <BrandName />
       </div>
     );
@@ -15,7 +16,7 @@ export const Logo = () => {
     return (
       <div id='name' className="pt-5 lg:pt-2">
         <h1 className="text-7xl font-extrabold lg:text-3xl">
-          Ezazi Classes
+        <Link to="/">Ezazi Classes</Link>  
         </h1>
       </div>
     );
