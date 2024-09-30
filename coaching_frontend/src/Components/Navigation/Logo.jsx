a9c034e3e30b9c3a9c71ea95d1ba1faa5d02163c
mohammad uvais <mohammaduvais517@gmail.com>
2024-09-30 20:26:21 +0530

    addded improvements

--- coaching_frontend/src/Components/Navigation/Logo.jsx
+++ coaching_frontend/src/Components/Navigation/Logo.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import logo from '../../components/SvgIcons/logo.svg';
-import { Link } from 'react-router-dom';
 
 
 export const Logo = () => {
     return (
       <div className="h-24 flex justify-center items-center gap-3">
-      <Link to="/"><img src={logo} alt="logo" className="w-24 lg:w-12 fill-white" /></Link>  
+        <img src={logo} alt="logo" className="w-24 lg:w-12 fill-white" />
         <BrandName />
       </div>
     );
@@ -16,7 +15,7 @@ export const Logo = () => {
     return (
       <div id='name' className="pt-5 lg:pt-2">
         <h1 className="text-7xl font-extrabold lg:text-3xl">
-        <Link to="/">Ezazi Classes</Link>  
+          Ezazi Classes
         </h1>
       </div>
     );
